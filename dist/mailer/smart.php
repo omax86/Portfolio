547f53dd9168f92c5a8ba4a7895612a0cb6263b2
Omax <omax1986@gmail.com>
2021-11-28 07:14:55 -0500

    init commit

---
+++ dist/mailer/smart.php
@@ -0,0 +1,44 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.domain.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'admin@omaxit.net';                 // Your login
+$mail->Password = '';                           	  // Email password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('admin@omaxit.net', 'Pulse');   // From whom email
+$mail->addAddress('admin@omaxit.net');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Contacts';
+$mail->Body    = '
+		User left their contacts <br> 
+	name: ' . $name . ' <br>
+	E-mail: ' . $email . ' <br.
+	text: ' . $text . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
---
+++ src/js/script.js
@@ -0,0 +1,63 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeElem = document.querySelector('.menu__close');
+const menuItem = document.querySelectorAll('.menu__item');
+const menuOverlay = document.querySelectorAll('.menu__overlay');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
+
+menuOverlay.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+
+
+$(document).ready(function(){
+
+    // Smooth scroll and page up
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('#pageup').fadeIn();
+        } else {
+            $('#pageup').fadeOut();
+        }
+    });
+
+    // Connect form to email
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "../mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function (){
+            $(this).find("input").val("");
+            $('.menu__overlay, #thanks').fadeIn('slow')
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+});
